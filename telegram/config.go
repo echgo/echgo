66915f287eb50cc23fa965b634e5a2fb7ebf098e
Jonas Kwiedor <jonas.kwiedor@jj-ideenschmiede.de>
2022-02-01 12:24:55 +0100

    Add telegram functions

--- telegram/config.go
+++ telegram/config.go
@@ -1,1 +1,48 @@
 package telegram
+
+import (
+	"bytes"
+	"net/http"
+)
+
+const baseUrl = "https://api.telegram.org/bot"
+
+// Config is to define config data
+type Config struct {
+	Path, Method string
+	ContentType  string
+	Body         []byte
+	AccessToken  bool
+	UploadMedia  bool
+}
+
+// Request is to define the request data
+type Request struct {
+	ApiToken string
+}
+
+// Send is to send a new request
+func (c *Config) Send(r Request) (*http.Response, error) {
+
+	// Set url
+	url := baseUrl + r.ApiToken + c.Path
+
+	// Define client
+	client := &http.Client{}
+
+	// Request
+	request, err := http.NewRequest(c.Method, url, bytes.NewBuffer(c.Body))
+	if err != nil {
+		return nil, err
+	}
+
+	// Send request & get response
+	response, err := client.Do(request)
+	if err != nil {
+		return nil, err
+	}
+
+	// Return data
+	return response, nil
+
+}
--- telegram/message.go
+++ telegram/message.go
@@ -1,1 +1,77 @@
 package telegram
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/url"
+)
+
+// CreateMessageReturn is to decode the json data
+type CreateMessageReturn struct {
+	Ok     bool `json:"ok"`
+	Result struct {
+		MessageId int `json:"message_id"`
+		From      struct {
+			Id        int64  `json:"id"`
+			IsBot     bool   `json:"is_bot"`
+			FirstName string `json:"first_name"`
+			Username  string `json:"username"`
+		} `json:"from"`
+		Chat struct {
+			Id                          int    `json:"id"`
+			Title                       string `json:"title"`
+			Type                        string `json:"type"`
+			AllMembersAreAdministrators bool   `json:"all_members_are_administrators"`
+		} `json:"chat"`
+		Date int    `json:"date"`
+		Text string `json:"text"`
+	} `json:"result"`
+}
+
+// CreateMessage is to create a message with a bot in a Telegram chat
+func CreateMessage(message, chatId, parseMode string, r Request) (CreateMessageReturn, error) {
+
+	// Set config for request
+	c := Config{
+		Path:   "/sendMessage",
+		Method: "GET",
+		Body:   nil,
+	}
+
+	// Parse url & add attributes
+	parse, err := url.Parse(c.Path)
+	if err != nil {
+		return CreateMessageReturn{}, err
+	}
+
+	newUrl, err := url.ParseQuery(parse.RawQuery)
+	if err != nil {
+		return CreateMessageReturn{}, err
+	}
+
+	newUrl.Add("chat_id", chatId)
+	newUrl.Add("parse_mode", parseMode)
+	newUrl.Add("text", message)
+
+	// Set new url
+	parse.RawQuery = newUrl.Encode()
+	c.Path = fmt.Sprintf("%s", parse)
+
+	// Send request
+	response, err := c.Send(r)
+	if err != nil {
+		return CreateMessageReturn{}, err
+	}
+
+	// Decode response
+	var decode CreateMessageReturn
+
+	err = json.NewDecoder(response.Body).Decode(&decode)
+	if err != nil {
+		return CreateMessageReturn{}, err
+	}
+
+	// Return data
+	return decode, nil
+
+}
