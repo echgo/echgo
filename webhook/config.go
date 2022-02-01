eef011f1282cc61dc182f8c6a80b65762a9f5b05
Jonas Kwiedor <jonas.kwiedor@jj-ideenschmiede.de>
2022-02-01 12:24:46 +0100

    Add webhook functions

--- webhook/config.go
+++ webhook/config.go
@@ -1,1 +1,46 @@
 package webhook
+
+import (
+	"bytes"
+	"net/http"
+)
+
+// Config is to define config data
+type Config struct {
+	Method string
+	Body   []byte
+}
+
+// Request is to define the request data
+type Request struct {
+	Domain string
+}
+
+// Send is to send a new request
+func (c *Config) Send(r Request) (*http.Response, error) {
+
+	// Set url
+	url := r.Domain
+
+	// Define client
+	client := &http.Client{}
+
+	// Request
+	request, err := http.NewRequest(c.Method, url, bytes.NewBuffer(c.Body))
+	if err != nil {
+		return nil, err
+	}
+
+	request.Header.Set("User-Agent", "echgo")
+	request.Header.Set("Content-Type", "application/json")
+
+	// Send request & get response
+	response, err := client.Do(request)
+	if err != nil {
+		return nil, err
+	}
+
+	// Return data
+	return response, nil
+
+}
--- webhook/send.go
+++ webhook/send.go
@@ -1,1 +1,38 @@
 package webhook
+
+import "encoding/json"
+
+// SendBody is to structure the body data
+type SendBody struct {
+	Headline string `json:"headline"`
+	Message  string `json:"message"`
+}
+
+// Send is to send the webhook
+func Send(body SendBody, r Request) error {
+
+	// Convert body
+	convert, err := json.Marshal(body)
+	if err != nil {
+		return err
+	}
+
+	// Set config for request
+	c := Config{
+		Method: "POST",
+		Body:   convert,
+	}
+
+	// Send request
+	response, err := c.Send(r)
+	if err != nil {
+		return err
+	}
+
+	// Close request
+	defer response.Body.Close()
+
+	// Return nothing
+	return nil
+
+}
