4501242dca2e651ae36d42a1ae74f6b6f53aab3c
Jonas Kwiedor <jonas.kwiedor@jj-ideenschmiede.de>
2022-02-02 09:55:02 +0100

    Update main function

--- main.go
+++ main.go
@@ -1,14 +1,30 @@
 package main
 
-import "github.com/echgo/echgo/console"
+import (
+	"github.com/echgo/echgo/configuration"
+	"github.com/echgo/echgo/console"
+	"github.com/echgo/echgo/cron"
+	"github.com/go-co-op/gocron"
+	"time"
+)
 
 func init() {
 
 	// Send init to console
 	console.Init()
 
+	// Import configuration
+	configuration.Import()
+
 }
 
 func main() {
+
+	// Define cronjobs &  start scheduler
+	s := gocron.NewScheduler(time.UTC)
+
+	s.Cron("* * * * *").Do(cron.Handler)
+
+	s.StartBlocking()
 
 }
