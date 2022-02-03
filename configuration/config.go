b5f22c356ecc575fc9cec7b95b32a76819f4bf99
Jonas Kwiedor <jonas.kwiedor@jj-ideenschmiede.de>
2022-02-03 11:32:14 +0100

    Update configuration struct

--- configuration/config.go
+++ configuration/config.go
@@ -3,8 +3,14 @@ package configuration
 // ConfigurationBody is to save & decode the yaml data
 type ConfigurationBody struct {
 	Cronjobs []struct {
-		Name string `yaml:"name"`
-		Cron string `yaml:"cron"`
+		Path         string `yaml:"path"`
+		Cron         string `yaml:"cron"`
+		Notification struct {
+			Gotify   bool `yaml:"gotify,omitempty"`
+			Telegram bool `yaml:"telegram,omitempty"`
+			SMTP     bool `yaml:"smtp,omitempty"`
+			Webhook  bool `yaml:"webhook,omitempty"`
+		} `yaml:"notification,omitempty"`
 	} `yaml:"cronjobs"`
 }
 
