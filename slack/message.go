e0d42319583ad4f691b1f1df4755424d44fa8c57
Jonas Kwiedor <jonas.kwiedor@jj-ideenschmiede.de>
2022-03-22 13:38:09 +0100

    Add request return message

--- slack/message.go
+++ slack/message.go
@@ -1,6 +1,9 @@
 package slack
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"io"
+)
 
 // CreateMessageBody is to structure the body data
 type CreateMessageBody struct {
@@ -8,11 +11,11 @@ type CreateMessageBody struct {
 }
 
 // CreateMessage is to create a message on a matrix server
-func CreateMessage(body CreateMessageBody, r Request) error {
+func CreateMessage(body CreateMessageBody, r Request) (string, error) {
 
 	convert, err := json.Marshal(body)
 	if err != nil {
-		return err
+		return "", err
 	}
 
 	c := Config{
@@ -22,11 +25,16 @@ func CreateMessage(body CreateMessageBody, r Request) error {
 
 	response, err := c.Send(r)
 	if err != nil {
-		return err
+		return "", err
 	}
 
 	defer response.Body.Close()
 
-	return nil
+	read, err := io.ReadAll(response.Body)
+	if err != nil {
+		return "", err
+	}
+
+	return string(read), nil
 
 }
