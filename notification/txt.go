0130f8aa279cb34f4ec82ebe7ea0d252e6503a89
Jonas Kwiedor <jonas.kwiedor@jj-ideenschmiede.de>
2022-04-01 18:07:51 +0200

    Update notification & channel handler

--- notification/txt.go
+++ notification/txt.go
@@ -23,33 +23,12 @@ func Txt(file *os.File) {
 
 	if len(parameter["channel"]) > 0 {
 
-		channel := channels.Type{}
+		var channel []string
 
 		split := strings.Split(parameter["channel"], ",")
 		for _, value := range split {
 			trim := strings.TrimSpace(value)
-			switch trim {
-			case "gotify":
-				channel.Gotify = true
-			case "matrix":
-				channel.Matrix = true
-			case "telegram":
-				channel.Telegram = true
-			case "discord":
-				channel.Discord = true
-			case "slack":
-				channel.Slack = true
-			case "trello":
-				channel.Trello = true
-			case "zendesk":
-				channel.Zendesk = true
-			case "osticket":
-				channel.Osticket = true
-			case "smtp":
-				channel.SMTP = true
-			case "webhook":
-				channel.Webhook = true
-			}
+			channel = append(channel, trim)
 		}
 
 		channels.Handler(parameter["headline"], parameter["message"], channel)
