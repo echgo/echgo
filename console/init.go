d1265543fc66a9b9c16ea8ce58c3066ab005ae88
Jonas Kwiedor <jonas.kwiedor@jj-ideenschmiede.de>
2022-05-21 11:20:01 +0200

    Update console function

--- console/init.go
+++ console/init.go
@@ -1,12 +1,15 @@
 package console
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 // Init is to create an initialized text
 func Init() {
 
-	fmt.Printf("\n*********************************************************************************\n")
+	fmt.Printf("\n%s\n", strings.Repeat("*", 80))
 	fmt.Printf("Welcome to echGo! Here you will find the most important information directly\nin the console. For more information visit https://github.com/echgo/echgo.\n")
-	fmt.Printf("*********************************************************************************\n\n")
+	fmt.Printf("%s\n\n", strings.Repeat("*", 80))
 
 }
