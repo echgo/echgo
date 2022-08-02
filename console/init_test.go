6e598b19398fac05874549c92b6a82d398ecd22d
Jonas Kwiedor <jonas.kwiedor@jj-ideenschmiede.de>
2022-08-02 15:45:28 +0200

    test: Add console test functions.
    On right location.

---
+++ console/init_test.go
@@ -0,0 +1,11 @@
+package console_test
+
+import (
+	"github.com/echgo/echgo/console"
+	"testing"
+)
+
+// TestInit is to test the console initialize function
+func TestInit(t *testing.T) {
+	console.Init()
+}
---
+++ console/log_test.go
@@ -0,0 +1,15 @@
+package console_test
+
+import (
+	"github.com/echgo/echgo/console"
+	"testing"
+)
+
+// TestLog is to test the console log function
+func TestLog(t *testing.T) {
+
+	attributes := make(map[string]any)
+	attributes["file_name"] = "test-file.txt"
+	console.Log("info", "A file was imported.", attributes)
+
+}
