d8d6b5251c70329ca8065c51eb5cfecd85bd6b35
Jonas Kwiedor <jonas.kwiedor@jj-ideenschmiede.de>
2022-08-03 15:25:01 +0200

    feat: Add use direct environment.

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/echgo/echgo/configuration"
 	"github.com/echgo/echgo/console"
+	"github.com/echgo/echgo/environment"
 	"github.com/echgo/echgo/notification"
 	"github.com/echgo/echgo/ticker"
 	"time"
@@ -13,11 +14,13 @@ func init() {
 	console.Init()
 }
 
-// Add dummy configuration file, if none exists
+// If no env we add a dummy configuration file, if none exists
 // And start ticker function with notification handler
 func main() {
 
-	configuration.Create()
+	if !environment.Boolean("USE_ENV") {
+		configuration.Create()
+	}
 
 	c := ticker.Config{Time: time.Now()}
 	c.Start(notification.Handler)
